0bf6de504bff81c6cf7dd8c6ceac42d6baae8c70
Yanni <yanni@192.168.124.14>
2020-03-15 11:10:47 +0800

    feat: twoSum

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# leetcode
+
+# leetCode top interview
+
+### 每周一道LeetCode算法题
+> 每周六晚8点，直播写题，哔哩哔哩房间号：21125453
+
+
+## top interview
+| 日期      | 答案 |
+| ----------- | ----------- |
+| 2020.3.14      | [leedcode1_twoSum.js](./leedcode1_twoSum.js)|
+
+
+
+
+
--- leedcode1_twoSum.js
+++ leedcode1_twoSum.js
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2020-03-14 20:03:18
- * @LastEditTime: 2020-03-14 21:45:33
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /undefined/Users/yanni/Desktop/leedcode1.js
- */
 // 1. 两数之和
 // 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 
@@ -16,7 +8,7 @@
 // 给定 nums = [2, 7, 11, 15], target = 9
 
 // 因为 nums[0] + nums[1] = 2 + 7 = 9
-// 所以返回 [0, 1]
+// 所以返回 [0, 1]  
 
 // 方法一
 function leedcodeOne(rawArr, target){
@@ -43,7 +35,6 @@ function leedcodeOne(rawArr, target){
 }
 let a = [230,863,916,585,981,404,316,785,88,12,70,435,384,778,887,755,740,337,86,92,325,422,815,650,920,125,277,336,221,847,168,23,677,61,400,136,874,363,394,199,863,997,794,587,124,321,212,957,764,173,314,422,927,783,930,282,306,506,44,926,691,568,68,730,933,737,531,180,414,751,28,546,60,371,493,370,527,387,43,541,13,457,328,227,652,365,430,803,59,858,538,427,583,368,375,173,809,896,370,789]
 let target = 542
-console.log(leedcodeOne(a,target))
 
 // 方法二
 function twoSum(nums, target){
